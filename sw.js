0384e5edfbf6210cf0fc816d1cb46c95c5bd2c19
ttttttt127127-ux <ttttttt127127@gmail.com>
2026-09-17 21:53:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@ const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/icon-180.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", e => {
